8d290aa9d1898642eaaae73fdc85bd461a8a3925
Vitaliy Samofal <vsamofal93@gmail.com>
2020-08-16 22:42:14 +0300

    updated scripts

--- all.sh
+++ all.sh
@@ -1,10 +1,18 @@
 ./chromium.sh
+sudo apt install -f
 ./google-chrome.sh
+sudo apt install -f
 ./guake.sh
+sudo apt install -f
 ./install-slack.sh
+sudo apt install -f
 ./telegram.sh
+sudo apt install -f
 ./vscode.sh
+sudo apt install -f
 ./archer-2tu-driver.sh
+sudo apt install -f
+./idea.sh
 
 #reboot is only because of archer 2tu here
 #sudo reboot
--- idea.sh
+++ idea.sh
@@ -1,3 +1,3 @@
 wget https://download-cf.jetbrains.com/idea/ideaIU-2020.2.tar.gz
 
-tar -xzvf ideaIU-2020.2.tar.gz ~/
+tar -xzvf ideaIU-2020.2.tar.gz -C ~/
